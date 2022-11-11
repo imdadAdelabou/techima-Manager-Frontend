43e98f3bebd37b9c7c0d0205a8a17c8ad2ddf128
imdadadelabou0 <imdadadelabou0@gmail.com>
2022-11-11 01:25:01 +0100

    [feat] REMOVE until package

--- src/main.ts
+++ src/main.ts
@@ -6,21 +6,12 @@ import { createPinia } from "pinia";
 import router from "./router";
 import axios from "axios";
 import VueAxios from "vue-axios";
-import { Quasar, Notify } from "quasar";
 
 const pinia = createPinia();
 
 const app = createApp(App);
 app.use(router);
 app.use(pinia);
-app.use(Quasar, {
-  plugins: {
-    Notify,
-  },
-  //   config: {
-  //     notify: {},
-  //   },
-});
 app.use(VueAxios, axios);
 app.provide("axios", app.config.globalProperties.axios);
 app.mount("#app");
