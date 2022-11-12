51cbcfc438c2dc2fcb559805523cf7cfcde185db
imdadadelabou0 <imdadadelabou0@gmail.com>
2022-11-12 20:22:15 +0100

    [feat] create an system for just user data when is required

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,56 @@
 <template>
-  <AppBar />
-  <router-view></router-view>
+  <div v-if="isLoading" class="flex justify-center h-screen items-center">
+    <SpinnerPage />
+  </div>
+  <div v-else>
+    <AppBar />
+    <router-view></router-view>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { createToast } from "mosha-vue-toastify";
+import { defineComponent, onBeforeMount, ref } from "vue";
 import AppBar from "./components/AppBar.vue";
 import SideBar from "./components/global/SideBar.vue";
+import SpinnerPage from "./components/global/SpinnerPage.vue";
+import { messages } from "./helpers/constants";
+import { User } from "./helpers/types";
+import router from "./router";
+import { dangerToastStyle, useUserStore } from "./stores/user";
 
 export default defineComponent({
-  components: { AppBar, SideBar },
+  components: { AppBar, SideBar, SpinnerPage },
   setup() {
-    return {};
+    const isLoading = ref(false);
+    const userStore = useUserStore();
+
+    onBeforeMount(async () => {
+      console.log("On App");
+      isLoading.value = true;
+
+      if (localStorage.getItem("userToken") == null) {
+        isLoading.value = false;
+        router.push("/");
+        return;
+      }
+
+      if (
+        userStore.$state.user.id != null &&
+        userStore.$state.user.id.length != 0
+      ) {
+        isLoading.value = false;
+        return;
+      }
+      let userData = await userStore.getDataOfUserConnected();
+      if (!userData) {
+        createToast(messages.errorOccurs, dangerToastStyle);
+        return;
+      }
+      userStore.$state.user = userData as User;
+      isLoading.value = false;
+    });
+    return { isLoading };
   },
 });
 </script>
